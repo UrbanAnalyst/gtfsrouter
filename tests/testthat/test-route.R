41072a4d22402f7b4ef86e7d722af6c99c1fc4f6
mpadge <mark.padgham@email.com>
2019-02-06 12:31:38 +0100

    turn off fread int64 in test data

--- tests/testthat/test-route.R
+++ tests/testthat/test-route.R
@@ -21,6 +21,19 @@ test_that ("timetable", {
               expect_true (file.exists (f))
               expect_silent (g <- extract_gtfs (f))
               expect_silent (gt <- gtfs_timetable (g))
+              expect_false (identical (g, gt))
+              expect_silent (gt2 <- gtfs_timetable (gt))
+              expect_identical (gt, gt2)
+              expect_true (length (gt) > length (g))
+              index <- which (names (gt) %in% names (g))
+              for (i in index)
+                  expect_identical (g [[i]], gt [[i]])
+              expect_equal (names (gt), c ("stop_times", "stops", "transfers",
+                                          "trip_table", "routes", "timetable",
+                                          "stations", "trip", "stop_ids",
+                                          "n_stations", "n_trips"))
+              expect_equal (gt$n_stations, 871)
+              expect_equal (gt$n_trips, 1933)
 })
 
 test_that("route", {
