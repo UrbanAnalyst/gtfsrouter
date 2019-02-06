d863f1d389b5ae5630034090178aea3950dfe02c
mpadge <mark.padgham@email.com>
2019-02-06 10:56:08 +0100

    :heavy_plus_sign:data:card_index:

--- R/gtfsrouter-package.R
+++ R/gtfsrouter-package.R
@@ -7,3 +7,27 @@
 #' @importFrom Rcpp evalCpp
 #' @useDynLib gtfsrouter, .registration = TRUE
 NULL
+
+#' berlin_gtfs
+#'
+#' Sample GTFS data from Verkehrsverbund Berlin-Brandenburg street, reduced to U
+#' and S Bahn only (underground and overground trains), and between the hours of
+#' 12:00-13:00. Only those components of the GTFS data necessary for routing
+#' have been retained.
+#'
+#' @name berlin_gtfs
+#' @docType data
+#' @keywords datasets
+#' @format A list of five \pkg{data.table} items necessary for routing:
+#' \itemize{
+#' \item routes
+#' \item trips
+#' \item stop_times
+#' \item stops
+#' \item transfers
+#' }
+#'
+#' @note Can be re-created with the script in 
+#' \url{https://github.com/ATFutures/gtfs-router/blob/master/data-raw/data-script.Rmd}.
+NULL
+
