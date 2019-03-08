729b62b613bda1f1b642046806be2a289b4bd02c
mpadge <mark.padgham@email.com>
2019-03-08 10:03:53 +0100

    add isochrone example

--- R/isochrone.R
+++ R/isochrone.R
@@ -21,6 +21,21 @@
 #' @inheritParams gtfs_route
 #'
 #' @return square matrix of distances between nodes
+#' @examples
+#' berlin_gtfs_to_zip ()
+#' f <- file.path (tempdir (), "vbb.zip")
+#' g <- extract_gtfs (f)
+#' g <- gtfs_timetable (g)
+#' from <- "Alexanderplatz"
+#' start_time <- 12 * 3600 + 600
+#' end_time <- start_time + 600
+#' ic <- gtfs_isochrone (g,
+#'                       from = from,
+#'                       start_time = start_time,
+#'                       end_time = end_time)
+#' \dontrun{
+#' plot (ic)
+#' }
 #' @export 
 gtfs_isochrone <- function (gtfs, from, start_time, end_time, day = NULL,
                             route_pattern = NULL, hull_alpha = 0.1, quiet = FALSE)
