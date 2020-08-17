c96d580155e5f2c5d1ee7d8ce93c6fbb9717d638
mpadge <mark.padgham@email.com>
2020-08-17 09:39:59 +0200

    mv plot.gtfs_isochrone to R/methods.R

--- R/isochrone.R
+++ R/isochrone.R
@@ -272,29 +272,3 @@ get_ahull <- function (x, alpha = alpha)
     }
     xy [match (ind_seq, xy$ind), ]
 }
-
-#' plot.gtfs_isochrone
-#'
-#' @name plot.gtfs_ischrone
-#' @param x object to be plotted
-#' @param ... ignored here
-#' @export
-plot.gtfs_isochrone <- function (x, ...)
-{
-    requireNamespace ("sf")
-    requireNamespace ("alphahull")
-    requireNamespace ("mapview")
-
-    allpts <- rbind (x$start_pt, x$mid_points, x$end_points)
-
-    m <- mapview::mapview (allpts, color = "grey", cex = 3, legend = FALSE)
-    m <- leafem::addFeatures (m, x$hull, color = "orange", alpha.regions = 0.2)
-    m <- leafem::addFeatures (m, x$routes, colour = "blue")
-    m <- leafem::addFeatures (m, x$start_point, radius = 5, color = "green")
-    m <- leafem::addFeatures (m, x$end_points, radius = 4, color = "red",
-                               fill = TRUE, fillOpacity = 0.8,
-                               fillColor = "red")
-
-    print (m)
-    invisible (m)
-}
