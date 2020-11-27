9302a4cc671748ad80b23fe5d94ec2c3b85f0bf1
mpadge <mark.padgham@email.com>
2020-11-27 13:02:30 +0100

    lint tests [ci skip]

--- tests/testthat/test-transfers.R
+++ tests/testthat/test-transfers.R
@@ -8,9 +8,10 @@ test_that ("transfers works", {
               f <- file.path (tempdir (), "vbb.zip")
               expect_silent (g <- extract_gtfs (f, quiet = TRUE))
 
-              expect_message (x200 <- gtfs_transfer_table (g,
-                                                           d_limit = 200,
-                                                           min_transfer_time = 0))
+              expect_message (
+                    x200 <- gtfs_transfer_table (g,
+                                                 d_limit = 200,
+                                                 min_transfer_time = 0))
 
               expect_is (x200, "data.table")
               expect_identical (names (x200), c ("from_stop_id",
@@ -20,9 +21,10 @@ test_that ("transfers works", {
               expect_true (all (x200$transfer_type == 2))
               expect_equal (min (x200$min_transfer_time), 0)
 
-              expect_message (x500 <- gtfs_transfer_table (g,
-                                                           d_limit = 500,
-                                                           min_transfer_time = 0))
+              expect_message (
+                    x500 <- gtfs_transfer_table (g,
+                                                 d_limit = 500,
+                                                 min_transfer_time = 0))
 
               expect_true (nrow (x500) > nrow (x200))
               expect_true (all (x200$from_stop_id %in% x500$from_stop_id))
