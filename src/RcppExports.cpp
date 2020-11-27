ae104f00c4e23cf86af3f943e455ab476c3c56cb
mpadge <mark.padgham@email.com>
2020-11-27 11:26:02 +0100

    mv csa-iso -> iso and split -> iso-rcpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -27,24 +27,6 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpp_csa_isochrone
-Rcpp::List rcpp_csa_isochrone(Rcpp::DataFrame timetable, Rcpp::DataFrame transfers, const size_t nstations, const size_t ntrips, const std::vector <size_t> start_stations, const int start_time, const int end_time, const bool minimise_transfers);
-RcppExport SEXP _gtfsrouter_rcpp_csa_isochrone(SEXP timetableSEXP, SEXP transfersSEXP, SEXP nstationsSEXP, SEXP ntripsSEXP, SEXP start_stationsSEXP, SEXP start_timeSEXP, SEXP end_timeSEXP, SEXP minimise_transfersSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< Rcpp::DataFrame >::type timetable(timetableSEXP);
-    Rcpp::traits::input_parameter< Rcpp::DataFrame >::type transfers(transfersSEXP);
-    Rcpp::traits::input_parameter< const size_t >::type nstations(nstationsSEXP);
-    Rcpp::traits::input_parameter< const size_t >::type ntrips(ntripsSEXP);
-    Rcpp::traits::input_parameter< const std::vector <size_t> >::type start_stations(start_stationsSEXP);
-    Rcpp::traits::input_parameter< const int >::type start_time(start_timeSEXP);
-    Rcpp::traits::input_parameter< const int >::type end_time(end_timeSEXP);
-    Rcpp::traits::input_parameter< const bool >::type minimise_transfers(minimise_transfersSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpp_csa_isochrone(timetable, transfers, nstations, ntrips, start_stations, start_time, end_time, minimise_transfers));
-    return rcpp_result_gen;
-END_RCPP
-}
 // rcpp_make_timetable
 Rcpp::DataFrame rcpp_make_timetable(Rcpp::DataFrame stop_times, std::vector <std::string> stop_ids, std::vector <std::string> trip_ids);
 RcppExport SEXP _gtfsrouter_rcpp_make_timetable(SEXP stop_timesSEXP, SEXP stop_idsSEXP, SEXP trip_idsSEXP) {
@@ -76,13 +58,31 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// rcpp_csa_isochrone
+Rcpp::List rcpp_csa_isochrone(Rcpp::DataFrame timetable, Rcpp::DataFrame transfers, const size_t nstations, const size_t ntrips, const std::vector <size_t> start_stations, const int start_time, const int end_time, const bool minimise_transfers);
+RcppExport SEXP _gtfsrouter_rcpp_csa_isochrone(SEXP timetableSEXP, SEXP transfersSEXP, SEXP nstationsSEXP, SEXP ntripsSEXP, SEXP start_stationsSEXP, SEXP start_timeSEXP, SEXP end_timeSEXP, SEXP minimise_transfersSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::DataFrame >::type timetable(timetableSEXP);
+    Rcpp::traits::input_parameter< Rcpp::DataFrame >::type transfers(transfersSEXP);
+    Rcpp::traits::input_parameter< const size_t >::type nstations(nstationsSEXP);
+    Rcpp::traits::input_parameter< const size_t >::type ntrips(ntripsSEXP);
+    Rcpp::traits::input_parameter< const std::vector <size_t> >::type start_stations(start_stationsSEXP);
+    Rcpp::traits::input_parameter< const int >::type start_time(start_timeSEXP);
+    Rcpp::traits::input_parameter< const int >::type end_time(end_timeSEXP);
+    Rcpp::traits::input_parameter< const bool >::type minimise_transfers(minimise_transfersSEXP);
+    rcpp_result_gen = Rcpp::wrap(rcpp_csa_isochrone(timetable, transfers, nstations, ntrips, start_stations, start_time, end_time, minimise_transfers));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_gtfsrouter_rcpp_convert_time", (DL_FUNC) &_gtfsrouter_rcpp_convert_time, 1},
     {"_gtfsrouter_rcpp_time_to_seconds", (DL_FUNC) &_gtfsrouter_rcpp_time_to_seconds, 1},
-    {"_gtfsrouter_rcpp_csa_isochrone", (DL_FUNC) &_gtfsrouter_rcpp_csa_isochrone, 8},
     {"_gtfsrouter_rcpp_make_timetable", (DL_FUNC) &_gtfsrouter_rcpp_make_timetable, 3},
     {"_gtfsrouter_rcpp_csa", (DL_FUNC) &_gtfsrouter_rcpp_csa, 8},
+    {"_gtfsrouter_rcpp_csa_isochrone", (DL_FUNC) &_gtfsrouter_rcpp_csa_isochrone, 8},
     {NULL, NULL, 0}
 };
 
