d5561e292a12b912144f591ed88f2a779c8572b1
mpadge <mark.padgham@email.com>
2021-11-04 13:55:23 +0100

    update RcppExports.cpp [ci skip]

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // rcpp_convert_time
 int rcpp_convert_time(const std::string& hms);
 RcppExport SEXP _gtfsrouter_rcpp_convert_time(SEXP hmsSEXP) {
