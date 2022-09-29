235b73879300f0333ede17e9e289a16ddd08f65a
mpadge <mark.padgham@email.com>
2022-09-29 14:47:20 +0200

    fix grep for freq_to_stop_times stop_ids in make_timetable (#89)

--- R/timetable.R
+++ R/timetable.R
@@ -118,7 +118,10 @@ make_timetable <- function (gtfs) {
     # trip_id values may be modified by rcpp_freqs_to_stop_times, by appending
     # "_[0-9]+", so need to grep for actual 'trip_id' values in 'stop_times'
     # table here:
-    index <- grep (paste0 (trip_ids, collapse = "|"), gtfs$stop_times$trip_id)
+    index <- lapply (unique (trip_ids), function (i) {
+        grep (i, gtfs$stop_times [, trip_id], fixed = TRUE)
+    })
+    index <- sort (unique (unlist (index)))
     trip_ids <- unique (gtfs$stop_times$trip_id [index])
     tt <- rcpp_make_timetable (gtfs$stop_times, stop_ids, trip_ids)
     # tt has [departure/arrival_station, departure/arrival_time,
@@ -181,9 +184,13 @@ filter_by_day <- function (gtfs, day = NULL, quiet = FALSE) {
     index <- which (gtfs$trips [, service_id] %in% service_id)
     gtfs$trips <- gtfs$trips [index, ]
     # trip_id values in stop_times can be modified by rcpp_freq_to_stop_times,
-    # which appends "_[0:9]+" values.
-    trip_ids <- paste0 (gtfs$trips [, trip_id], collapse = "|")
-    index <- grep (trip_ids, gtfs$stop_times [, trip_id])
+    # which appends "_[0:9]+" values. The grepped patterns can't be joined
+    # together, as too many "|" conditions can run out of memory on a grep call.
+    # Luckily, using lapply on grep with "fixed = TRUE" is pretty fast.
+    index <- lapply (unique (gtfs$trips [, trip_id]), function (i) {
+        grep (i, gtfs$stop_times [, trip_id], fixed = TRUE)
+    })
+    index <- sort (unique (unlist (index)))
     gtfs$stop_times <- gtfs$stop_times [index, ]
 
     return (gtfs)
