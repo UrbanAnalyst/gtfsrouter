131be83e38117fdf7ca0c0bc86f390804d489289
mpadge <mark.padgham@email.com>
2023-08-16 13:41:54 +0200

    switch all examples back on but with DTthreads=1; close #109

--- R/transfers.R
+++ R/transfers.R
@@ -18,12 +18,13 @@
 #' @return Modified version of the `gtfs` input with additional transfers table.
 #'
 #' @examples
+#' # Examples must be run on single thread only:
+#' data.table::setDTthreads (1)
+#'
 #' berlin_gtfs_to_zip ()
 #' f <- file.path (tempdir (), "vbb.zip")
 #' g <- extract_gtfs (f, quiet = TRUE)
-#' \dontrun{
 #' g <- gtfs_transfer_table (g, d_limit = 200)
-#' }
 #' # g$transfers then has fewer rows than original, because original transfer
 #' # table contains duplicated rows.
 #' @family augment
